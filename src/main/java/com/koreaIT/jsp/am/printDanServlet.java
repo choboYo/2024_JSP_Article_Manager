e8772e4d795bcb4a4beeeb056767b3e500dae04b
choboYo <tjddlsmn96@gmail.com>
2024-06-11 19:10:29 +0900

    DBConnectTest, ArticleListServlet 진행중

--- src/main/java/com/koreaIT/jsp/am/printDanServlet.java
+++ src/main/java/com/koreaIT/jsp/am/printDanServlet.java
@@ -8,12 +8,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/home/main")
-public class HomeMainServlet extends HttpServlet {
+@WebServlet("/printDan")
+public class printDanServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Hello World~~~~!!!!");
+		request.getRequestDispatcher("/jsp/home/printDan.jsp").forward(request, response);
 	}
-
 }
